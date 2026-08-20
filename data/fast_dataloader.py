22208aad2b209cdba8df3b61a1a261de46ecc994
Markus Mueller <markus.mueller939@gmail.com>
2026-08-20 10:43:22 +0200

    fix drop_last in fasttensordataloader

--- data/fast_dataloader.py
+++ data/fast_dataloader.py
@@ -1,51 +1,43 @@
 import torch
 
+
 class FastTensorDataLoader:
-    """
-    A DataLoader-like object for a set of tensors that can be much faster than
+    """A DataLoader-like object for a set of tensors that can be much faster than
     TensorDataset + DataLoader because dataloader grabs individual indices of
     the dataset and calls cat (slow).
+
     Adapted from: https://discuss.pytorch.org/t/dataloader-much-slower-than-manual-batching/27014/6
     """
 
-    def __init__(self, *data, batch_size=256, shuffle=False, drop_last=False):
-        assert all(d.shape[0] == data[0].shape[0] for d in data), "All tensors must have the same size in the first dimension"
-        self.dataset_len = data[0].shape[0]
+    def __init__(self, *data, batch_size=32, shuffle=False, drop_last=False):
+        self.dataset_len = next(t.shape[0] for t in data if t is not None)
+        assert all(t.shape[0] == self.dataset_len for t in data if t is not None)
         self.data = data
         self.batch_size = batch_size
         self.shuffle = shuffle
+        self.drop_last = drop_last
 
-        if drop_last:
-            self.dataset_len = (self.dataset_len // self.batch_size) * self.batch_size
-
-        # Calculate # batches
         n_batches, remainder = divmod(self.dataset_len, self.batch_size)
-        if remainder > 0:
-            n_batches += 1
-        self.n_batches = n_batches
+        self.n_batches = n_batches if drop_last else n_batches + (remainder > 0)
+        self.iter_len = self.n_batches * self.batch_size if drop_last else self.dataset_len
 
     def __iter__(self):
-        if self.shuffle:
-            self.indices = torch.randperm(self.dataset_len)
-        else:
-            self.indices = None
+        self.indices = torch.randperm(self.dataset_len) if self.shuffle else None
         self.i = 0
         return self
 
     def __next__(self):
-        if self.i >= self.dataset_len:
+        if self.i >= self.iter_len:
             raise StopIteration
 
         if self.indices is not None:
-            idx = self.indices[self.i : self.i + self.batch_size]
-            batch = tuple(torch.index_select(d, 0, idx) for d in self.data)
+            indices = self.indices[self.i : self.i + self.batch_size]
+            batch = tuple(torch.index_select(t, 0, indices) if t is not None else None for t in self.data)
         else:
-            batch = tuple(d[self.i : self.i + self.batch_size] for d in self.data)
+            batch = tuple(t[self.i : self.i + self.batch_size] if t is not None else None for t in self.data)
 
         self.i += self.batch_size
         return batch
 
     def __len__(self):
         return self.n_batches
-
-
